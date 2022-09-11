8557f6f259038caee666bcb5441b28b8a16233ef
BRO-IT <113203373+BRO-IT@users.noreply.github.com>
2022-09-11 20:27:26 +0300

    Update ProgressBar_Usage.go

--- ProgressBar_Usage.go
+++ ProgressBar_Usage.go
@@ -1,54 +1,94 @@
 package main
 
 import (
-	"time"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
-	"fyne.io/fyne/v2/widget"
+  "fyne.io/fyne/v2"
+  "fyne.io/fyne/v2/app"
+  "fyne.io/fyne/v2/container"
+  "fyne.io/fyne/v2/dialog"
+  "fyne.io/fyne/v2/widget"
 )
 
 // BRO-IT
 
 func main() {
-	a := app.New()
-	w := a.NewWindow("Подпишись на канал :)")
-	w.Resize(fyne.NewSize(650, 650))
+  a := app.New()
+  w := a.NewWindow("Подпишись на канал :)")
+  w.Resize(fyne.NewSize(650, 650))
 
-	pbinf := widget.NewProgressBarInfinite()
-	pbinf.Hide()
+  title := widget.NewLabel("Sign Up Progress")
 
-	title := widget.NewLabel("Create your post")
+  pb := widget.NewProgressBar()
+  pb.Max = 100.0
 
-	post_title := widget.NewEntry()
-	post_title.SetPlaceHolder("Your post title")
+  name := widget.NewEntry()
+  name.SetPlaceHolder("Your name")
 
-	post_text := widget.NewMultiLineEntry()
-	post_text.SetPlaceHolder("Your post text")
+  surname := widget.NewEntry()
+  surname.SetPlaceHolder("Your surname")
 
-	submit := widget.NewButton("Submit", func() {
-		pbinf.Show()
-		time.Sleep(time.Second * 3)
-		pbinf.Hide()
+  phone_number := widget.NewEntry()
+  phone_number.SetPlaceHolder("Your phone number")
 
-		dialog.ShowInformation(
-			"Post Creation",
-			"You have created your post!",
-			w,
-		)
-	})
+  nick := widget.NewEntry()
+  nick.SetPlaceHolder("Your nick")
 
-	w.SetContent(
-		container.NewVBox(
-			pbinf,
-			title,
-			post_title,
-			post_text,
-			submit,
-		),
-	)
-	w.Show()
-	a.Run()
+  password := widget.NewPasswordEntry()
+  password.SetPlaceHolder("Your password")
+
+  email := widget.NewEntry()
+  email.SetPlaceHolder("Your email")
+
+  bio := widget.NewMultiLineEntry()
+  bio.SetPlaceHolder("Info about you")
+
+  tabs := container.NewAppTabs(
+    container.NewTabItem(
+      "Personal Info",
+      container.NewVBox(
+        name,
+        surname,
+        phone_number,
+      ),
+    ),
+    container.NewTabItem(
+      "Data for Sign Up",
+      container.NewVBox(
+        nick,
+        password,
+      ),
+    ),
+    container.NewTabItem(
+      "Additional Info",
+      container.NewVBox(
+        email,
+        bio,
+      ),
+    ),
+  )
+
+  next := widget.NewButton("Next", func() {
+    if tabs.SelectedIndex() < 2 {
+      tabs.SelectIndex(tabs.SelectedIndex() + 1)
+      pb.SetValue(pb.Value + 33)
+    } else {
+      pb.SetValue(pb.Value + 34)
+
+      dialog.ShowInformation(
+        "Sign Up",
+        "You've beeen signed up!",
+        w,
+      )
+    }
+  })
+
+  w.SetContent(
+    container.NewVBox(
+      title,
+      pb,
+      tabs,
+      next,
+    ),
+  )
+  w.Show()
+  a.Run()
 }
