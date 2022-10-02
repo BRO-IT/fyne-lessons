0022fa8bba32895f39d420e37101e4d5383bf439
BRO-IT <113203373+BRO-IT@users.noreply.github.com>
2022-10-02 19:23:16 +0300

    Fyne #34. Example #1(using widget.Table) 👍
    Пример использования виджет Table в Fyne 👍

---
+++ Table_Example_#1.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+  "fyne.io/fyne/v2"
+  "fyne.io/fyne/v2/app"
+  "fyne.io/fyne/v2/widget"
+)
+
+// BRO-IT
+
+func main() {
+  a := app.New()
+  w := a.NewWindow("SUBSCRIBE :)")
+  w.Resize(fyne.NewSize(500, 500))
+
+  data := [][]string{
+    {"John", "12", "12", "12"},
+    {"Andrew", "11", "11", "11"},
+    {"Kate", "10", "10", "10"},
+  }
+
+  table := widget.NewTable(
+    func() (int, int) {
+      return len(data), len(data[0])
+    },
+
+    func() fyne.CanvasObject {
+      return widget.NewLabel("Default text")
+    },
+
+    func(tci widget.TableCellID, co fyne.CanvasObject) {
+      co.(*widget.Label).SetText(data[tci.Row][tci.Col])
+    },
+  )
+
+  w.SetContent(table)
+  w.Show()
+  a.Run()
+}
